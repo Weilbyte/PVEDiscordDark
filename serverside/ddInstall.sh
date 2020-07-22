31f59b01eb13efe2791fec32ae00620500cc6f48
Weilbyte <git@weilbyte.dev>
2020-07-22 11:48:06 +0200

    Create ddInstall.sh

---
+++ serverside/ddInstall.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+Say () {
+    printf "\e[1;34m $1  \e[0m \n";
+}
+
+DotSay () {
+    printf "[\e[1;34m*\e[0m] \e[1;34m $1  \e[0m \n"; 
+}
+
+
+Say '[PVE Discord Dark UI Theme Installer - OLD VERSION (DEPRECATED)]'
+Say 'Internet connection required to download files'
+Say '>Press any key to begin installation'
+read -p ""
+Say ' '
+DotSay 'Backing up index template file'
+cp /usr/share/pve-manager/index.html.tpl /usr/share/pve-manager/index.html.tpl.bak 
+DotSay 'Applying stylesheet..'
+echo "<link rel='stylesheet' type='text/css' href='/pve2/css/dd_style.css'>" >> /usr/share/pve-manager/index.html.tpl
+echo "<script type='text/javascript' src='/pve2/js/dd_patcher.js'></script>" >> /usr/share/pve-manager/index.html.tpl
+cd /usr/share/pve-manager/css
+wget -O dd_style.css https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/old/serverside/style.css &> /dev/null 
+cd /usr/share/pve-manager/js
+wget -O dd_patcher.js https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark/js/PVEDiscordDark.js &> /dev/null
+DotSay 'Applied stylesheet!'
+DotSay 'Downloading images..'
+cd /usr/share/pve-manager/images
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_cephblurp.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_cephwhite.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-cpu.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-hdd.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-ram.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-swap.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-cd.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-display.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-network.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-cloud.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-serial.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-usb.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_icon-pci.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_logo.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_mini-bottom.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_mini-top.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_readme &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_tool-sprites.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_trigger.png &> /dev/null 
+wget https://github.com/Weilbyte/PVEDiscordDark/raw/old/images/dd_loading.svg &> /dev/null 
+
+DotSay 'Downloaded images!'
+Say ''
+Say 'Installation finished!'
+Say 'o7'  
