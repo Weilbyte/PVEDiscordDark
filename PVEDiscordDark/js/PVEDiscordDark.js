2d8679eed5637ec566f08d2b030729e64c3b7921
Luckyvb <Luckyvb@users.noreply.github.com>
2021-06-10 00:33:20 +0300

    logo image path fix

--- PVEDiscordDark/js/PVEDiscordDark.js
+++ PVEDiscordDark/js/PVEDiscordDark.js
@@ -10,7 +10,7 @@ const swapLogo = async function() {
 			node.parentElement.parentElement.style.background = '#23272A';
 			node.setAttribute('height', `${height}px`);
 			node.setAttribute('width', `${width}px`);
-			node.setAttribute('src', '/pve2/images/dd_logo.png');
+			node.setAttribute('src', '/images/dd_logo.png');
 		}
 	}
 	if (!found) {
